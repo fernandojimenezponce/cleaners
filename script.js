1daab9a00493fc39150b284af083752de654957c
Ponce <fponce@fponce01scl.attlocal.net>
2020-01-19 00:34:53 -0800

    gallery done

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 "use strict";
+
+var msnry = new Masonry( '.gallery-grid', {
+    columnWidth: '.grid-sizer',
+    itemSelector: '.gallery-item',
+    percentPosition: true,
+});
+
+$(document).ready(function(){
+    $('.gallery-carousel').slick({
+        dots: true,
+        centerMode: true,
+        centerPadding: '30px',
+        slidesToShow: 1,
+        arrows: false,
+    });
+  });
